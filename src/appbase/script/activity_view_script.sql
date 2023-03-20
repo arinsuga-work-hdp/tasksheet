6b4149a7432e85e36b3e6ff7db365ab7d65cb2c1
unknown <arinsuga@gmail.com>
2023-03-20 16:42:37 +0700

    dev2.0 - dashboard: wrk

--- src/appbase/script/activity_view_script.sql
+++ src/appbase/script/activity_view_script.sql
@@ -1,19 +1,18 @@
 drop view activity_view;
+
 create view activity_view as
 select a.id,
 CAST(a.created_at as DATE) as activity_dt,
-a.created_by, b.name,
+YEAR(a.created_at) as activity_year,
+MONTH(a.created_at) as activity_month,
+CAST(CONCAT(CAST(YEAR(a.created_at) as CHAR), CAST(MONTH(a.created_at) as CHAR)) AS UNSIGNED) as activity_yearmonth,
+a.created_by, b.name as created_byname,
 a.activitytype_id, a1.name as activitytype_name,
 a.activitysubtype_id, a2.name as activitysubtype_name,
 a.activitystatus_id,
 a.tasktype_id, a3.name as tasktype_name,
 a.tasksubtype1_id, a4.name as tasksubtype1_name,
 a.tasksubtype2_id, a5.name as tasksubtype2_name,
-/* ALL_OPEN_CLOSE_PENDING_CANCEL */
-IF(a.activitystatus_id = 1, 1, 0) as all_open,
-IF(a.activitystatus_id = 2, 1, 0) as all_close,
-IF(a.activitystatus_id = 3, 1, 0) as all_pending,
-IF(a.activitystatus_id = 4, 1, 0) as all_cancel,
 /* SUPPORT */
 IF(a.activitytype_id = 1,
   IF(a.activitysubtype_id = 1,
@@ -21,26 +20,8 @@ IF(a.activitytype_id = 1,
 IF(a.activitytype_id = 1,
   IF(a.activitysubtype_id = 2,
       IF(a.activitystatus_id = 1, 1, 0), 0), 0) as support_incident_open,
-/* MAINTENANCE */
-IF(a.activitytype_id = 2,
-  IF(a.activitysubtype_id = 3,
-     IF(a.activitystatus_id = 1, 1, 0), 0), 0) as maintenance_reguler_open,
-IF(a.activitytype_id = 2,
-  IF(a.activitysubtype_id = 4,
-     IF(a.activitystatus_id = 1, 1, 0), 0), 0) as maintenance_request_open,
-IF(a.activitytype_id = 2,
-  IF(a.activitysubtype_id = 5,
-     IF(a.activitystatus_id = 1, 1, 0), 0), 0) as maintenance_incident_open,
-/* PROJECT*/
-IF(a.activitytype_id = 3,
-  IF(a.tasktype_id = 16,
-     IF(a.activitystatus_id = 1, 1, 0), 0), 0) as project_hardware_open,
-IF(a.activitytype_id = 3,
-  IF(a.tasktype_id = 17,
-     IF(a.activitystatus_id = 1, 1, 0), 0), 0) as project_software_open,
-IF(a.activitytype_id = 3,
-  IF(a.tasktype_id = 18,
-     IF(a.activitystatus_id = 1, 1, 0), 0), 0) as project_network_open
+/* SUPPORT */
+IF(a.activitytype_id = 1, 1, 0) as support_all
 from activity a
 left outer join activitytype a1 on a.activitytype_id = a1.id
 left outer join activitysubtype a2 on a.activitysubtype_id = a2.id
