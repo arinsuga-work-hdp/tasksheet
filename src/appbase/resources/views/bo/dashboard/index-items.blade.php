166c4cb0aee26425bd0e3d380ef513f6129c26d9
unknown <arinsuga@gmail.com>
2023-03-28 16:24:46 +0700

    dev2.0 - dashboard ui vs be: wrk

--- src/appbase/resources/views/bo/dashboard/index-items.blade.php
+++ src/appbase/resources/views/bo/dashboard/index-items.blade.php
@@ -1,39 +1,44 @@
 
-
+<!-- TICKET DAN ABSENSI -->
 <div class="row">
-    <div class="col-lg-6 col-sm-12">
+    <div class="col-lg-5 col-sm-12">
         <!-- small box -->
         <div class="small-box bg-primary">
             <div class="inner">
                 <h3>Open Ticket</h3>
 
-                <div class="row">
-                    <div class="col-md-2">
-                        Incident
+                <p>
+                    <div class="row">
+                        <div class="col-md-2">
+                            Incident
+                        </div>
+                        <div class="col-md-10">
+                            : {{ $viewModel->data->ticket->incident }}
+                        </div>
                     </div>
-                    <div class="col-md-10">
-                        : {{ $viewModel->data->ticket->incident }}
-                    </div>
-                </div>
+                </p>
 
-                <div class="row">
-                    <div class="col-md-2">
-                        Request
+                <p>
+                    <div class="row">
+                        <div class="col-md-2">
+                            Request
+                        </div>
+                        <div class="col-md-10">
+                            : {{ $viewModel->data->ticket->request }}
+                        </div>
                     </div>
-                    <div class="col-md-10">
-                        : {{ $viewModel->data->ticket->request }}
-                    </div>
-                </div>
+                </p>
 
-                <div class="row">
-                    <div class="col-md-2">
-                        Pending
+                <p>
+                    <div class="row">
+                        <div class="col-md-2">
+                            Pending
+                        </div>
+                        <div class="col-md-10">
+                            : {{ $viewModel->data->ticket->pending }}
+                        </div>
                     </div>
-                    <div class="col-md-10">
-                        : {{ $viewModel->data->ticket->pending }}
-                    </div>
-                </div>
-
+                </p>
             </div>
             <div class="icon">
                 <i class="fa fa-ticket"></i>
@@ -41,35 +46,52 @@
         </div>
     </div>
 
-    <div class="col-lg-6 col-sm-12">
-        <!-- small box -->
-        <div class="small-box bg-warning">
-            <div class="inner">
-                <h3>150</h3>
+    <div class="col-lg-7 col-sm-12">
+        <div class="card bg-warning">
+            <div class="card-header">
+                <h3 class="card-title">Absen Karyawan</h3>
+            </div>
 
-                <p>Maintenance</p>
+            <div class="card-body table-responsive p-0" style="height: 300px;">
+
             </div>
-            <div class="icon">
-                <i class="fa fa-tools"></i>
-            </div>
-            <a href="#" class="small-box-footer">View Detail <i class="fas fa-arrow-circle-right"></i></a>
         </div>
     </div>
 </div>
 
-<div class="row">
+<!-- MY TASK -->
+<div class="row" style="height: ">
     <div class="col-lg-12 col-sm-12">
-        <!-- small box -->
-        <div class="small-box bg-pink">
-            <div class="inner">
-                <h3>5</h3>
-                <p>My Task: Task yang sedang dikerjakan oleh user sesuai login</p>
-            </div>
-            <div class="icon">
-                <i class="fa fa-tasks"></i>
+        <div class="card bg-pink">
+            <div class="card-header">
+                <h3 class="card-title">My Task</h3>
             </div>
 
-            <a href="#" class="small-box-footer">View Detail <i class="fas fa-arrow-circle-right"></i></a>
+            <div class="card-body table-responsive p-0" style="height: 300px;">
+
+            <table class="table table-head-fixed text-nowrap">
+                  <thead>
+                    <tr class="bg-pink">
+                      <th class="bg-pink">ID</th>
+                      <th class="bg-pink">User</th>
+                      <th class="bg-pink">Date</th>
+                      <th class="bg-pink">Status</th>
+                      <th class="bg-pink">Reason</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>183</td>
+                      <td>John Doe</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-success">Approved</span></td>
+                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+
+            </div>
         </div>
     </div>
 </div>
