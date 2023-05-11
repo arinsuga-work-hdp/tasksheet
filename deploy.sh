9930742521a58b7f1bb98416872b943feef49c87
unknown <arinsuga@gmail.com>
2023-05-11 16:39:22 +0700

    v2.1 - tambah setting database

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ php artisan config:clear
 php artisan view:clear
 php artisan route:clear
 composer install --optimize-autoloader
+composer dumpautoload
 cd public
 rm -R storage
 ln -s ../storage/app/public storage
